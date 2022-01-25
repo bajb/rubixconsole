f2c4167d3de1494319405777906929ff6da8f744
Kyle Essex <kylegavinessex@hotmail.com>
2022-01-25 10:30:19 +0000

    Fixes

--- rollup.config.js
+++ rollup.config.js
@@ -1,9 +1,7 @@
 import commonjs from '@rollup/plugin-commonjs';
 import resolve from '@rollup/plugin-node-resolve';
-import typescript from '@rollup/plugin-typescript';
 import {terser} from 'rollup-plugin-terser';
 import babel from '@rollup/plugin-babel';
-import babelClassProperties from '@babel/plugin-proposal-class-properties';
 
 // `npm run build` -> `production` is true
 // `npm run dev` -> `production` is false
@@ -11,29 +9,12 @@ const production = !process.env.ROLLUP_WATCH;
 
 const commonPlugins = [
   resolve({browser: true, preferBuiltins: false}), // tells Rollup how to find node_modules packages
-  typescript(), // run typescript compiler
   commonjs(), // converts date-fns to ES modules
-  babel(
-    {
-      babelHelpers: 'bundled',
-      babelrc:      false,
-      exclude:      [/\/core-js\//],
-      presets:      [
-        [
-          '@babel/preset-env',
-          {
-            corejs:      3,
-            modules:     false,
-            useBuiltIns: 'usage'
-          }
-        ]
-      ],
-      plugins:      [babelClassProperties]
-    }),
+  babel({babelHelpers: 'bundled'}),
   production && terser() // minify, but only in production
 ];
 
-const console = {
+export default {
   input:   'src/console.ts',
   output:  {
     dir:            'static',
@@ -43,4 +24,3 @@ const console = {
   },
   plugins: [...commonPlugins]
 };
-export default [console];
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,8 @@
 module.exports = {
 
-  content: ["./*.{html,js}"],
+  content: ['./*.html'],
   theme:   {
-    extend: {},
+    extend: {}
   },
-  plugins: [],
-}
+  plugins: []
+};
