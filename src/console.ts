4780c72b26db08a34665b7fec84dddc418a75d62
Brooke Bryan <brooke@bajb.net>
2022-01-27 09:02:51 +0000

    Clean up app sidebar on mobile/desktop

--- src/console.ts
+++ src/console.ts
@@ -1,11 +1,3 @@
 import Alpine from 'alpinejs'
-import {on} from '@packaged-ui/fusion/src/Foundation/res/events.js'
-
 import './console.scss';
-
-
 Alpine.start();
-
-on(document, 'click', 'h1', (e) => {
-    console.log(e)
-})
