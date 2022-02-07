97fa42c6925a19c47157f7921e645838db29cc69
Kyle Essex <kylegavinessex@hotmail.com>
2022-02-07 16:35:37 +0000

    fix app nav menu

--- src/console.ts
+++ src/console.ts
@@ -1,9 +1,10 @@
 import Alpine from 'alpinejs'
 import './console.scss';
-import CompanionApp from "./companionApp";
+import CompanionApp from "./Alpine/companionApp";
 
 window.addEventListener('alpine:init', function () {
     Alpine.data('companionApp', CompanionApp);
+
 });
 
 Alpine.start();
